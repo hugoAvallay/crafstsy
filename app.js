ad5b3983492a02cf8b5979aeda83ff5ff9b2e87c
havallay <h40hugo@gmail.com>
2023-07-26 20:16:29 -0300

    feat: add file styles

--- app.js
+++ app.js
@@ -7,9 +7,14 @@ const PORT = 3030;
 app.use(express.static('public'));
 
 
-app.get("/", (req, res) =>
-    res.sendFile(path.join(__dirname, "views", "index.html"))
-);
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "views/index.html")));
+
+app.get("/registro", (req, res) => res.sendFile(path.join(__dirname, "views/partials/register.html")));
+
+app.get('/login', (req,res)=> res.sendFile(path.join(__dirname,'views','partials', 'login.html')));
+
 
 
 app.listen(PORT, () => console.log(`Server running in http://localhost:${PORT}`));
+
+
